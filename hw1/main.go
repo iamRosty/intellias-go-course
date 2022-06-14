3c5e0db8a62aa143983d05346abde7684328c5bb
Rostyslav Lysenko <educ@i.ua>
2022-06-14 21:47:33 +0300

    added some consts

--- hw1/main.go
+++ hw1/main.go
@@ -3,14 +3,16 @@ package main
 import "fmt"
 
 const (
-	applePrice = 5.99
-	pearPrice  = 7.
-	myBudget   = 23.
+	applePrice   = 5.99
+	pearPrice    = 7.
+	myBudget     = 23.
+	numberApples = 9
+	numberPears  = 8
 )
 
 func main() {
 	fmt.Printf("Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?\n")
-	necessaryAmount := applePrice*9 + pearPrice*8
+	necessaryAmount := applePrice*numberApples + pearPrice*numberPears
 	fmt.Printf("Щоб купити 9 яблук та 8 груш, нам потрiбно %.2f\n", necessaryAmount)
 	fmt.Printf("Скільки груш ми можемо купити?\n")
 	numberPearsWeCanBuy := myBudget / pearPrice
