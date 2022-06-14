df97fa97350aa5ed978c2860f1d4f2dea73fee17
Rostyslav Lysenko <educ@i.ua>
2022-06-14 21:53:58 +0300

    added some consts

--- hw1/main.go
+++ hw1/main.go
@@ -11,13 +11,13 @@ const (
 )
 
 func main() {
-	fmt.Printf("Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?\n")
+	fmt.Println("Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?")
 	necessaryAmount := applePrice*numberApples + pearPrice*numberPears
-	fmt.Printf("Щоб купити 9 яблук та 8 груш, нам потрiбно %.2f\n", necessaryAmount)
-	fmt.Printf("Скільки груш ми можемо купити?\n")
+	fmt.Println("Щоб купити 9 яблук та 8 груш, нам потрiбно", necessaryAmount)
+	fmt.Println("Скільки груш ми можемо купити?")
 	numberPearsWeCanBuy := myBudget / pearPrice
 	fmt.Printf("Ми можемо купити груш: %v шт.\n", int(numberPearsWeCanBuy))
-	fmt.Printf("Скільки яблук ми можемо купити?\n")
+	fmt.Println("Скільки яблук ми можемо купити?")
 	numberApplesWeCanBuy := myBudget / pearPrice
 	fmt.Printf("Ми можемо купити яблук: %v шт.\n", int(numberApplesWeCanBuy))
 	canBuy := myBudget >= 2*(pearPrice+applePrice) //Чи можемо ми купити 2 груші та 2 яблука?
