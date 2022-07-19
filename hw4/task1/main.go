7ce626a2dd836ddbae25aa0bb97b16a9b12bf924
Rostyslav Lysenko <educ@i.ua>
2022-07-19 18:17:36 +0300

    added concurrency comput

--- hw4/task1/main.go
+++ hw4/task1/main.go
@@ -1,15 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-// Конкурентно порахувати суму кожного слайсу int, та роздрукувати результат.
-// Потрібно використовувати WaitGroup.
-// Приклад:
-// [ [ 4, 6 ], [ 7, 9 ] ]
-// Результат друку:
-// Порядок друку не важливий.
-// “slice 1: 10”
-// “slice 2: 16”
 func main() {
 	n := [][]int{
 		{2, 6, 9, 24},
@@ -17,35 +12,21 @@ func main() {
 		{4, 2, 8, 35},
 	}
 
-	sum(n)
+	wg := sync.WaitGroup{}
+	wg.Add(len(n))
+	for i := 0; i < len(n); i++ {
+		s := n[i]
+		go func([]int) {
+			defer wg.Done()
+			sum(s)
+		}(s)
+	}
+	wg.Wait()
 }
-
-func sum(slc [][]int) {
-	firstAddGoroutine(slc[0])
-	secondAddGoroutine(slc[1])
-	thirdAddGoroutine(slc[2])
-}
-
-func firstAddGoroutine(slc []int) {
+func sum(slc []int) {
 	sum := 0
 	for _, v := range slc {
 		sum += v
 	}
-	fmt.Println("slice 1: ", sum)
-}
-
-func secondAddGoroutine(slc []int) {
-	sum := 0
-	for _, v := range slc {
-		sum += v
-	}
-	fmt.Println("slice 2: ", sum)
-}
-
-func thirdAddGoroutine(slc []int) {
-	sum := 0
-	for _, v := range slc {
-		sum += v
-	}
-	fmt.Println("slice 3: ", sum)
+	fmt.Println("slice elements sum: ", sum)
 }
